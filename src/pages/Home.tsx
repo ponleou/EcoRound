f0138a71ad1cde881ecd858d76a939f92ae41c00
Ponleou Keo Sok <ponleousk@gmail.com>
2024-12-07 23:49:15 +1100

    feat: added bottom nav in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonFooter,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonIcon,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonTabs,
+  IonTab,
+  IonTabBar,
+  IonTabButton,
+} from "@ionic/react";
+import {
+  homeOutline,
+  personCircleOutline,
+  locationOutline,
+  ellipsisVertical,
+} from "ionicons/icons";
+import "./Home.css";
 
-const Home: React.FC = () => {
+export default function Home() {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader translucent={true}>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>EcoRound</IonTitle>
+
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
       </IonContent>
+
+      <IonTabBar>
+        <IonTabButton>
+          <IonIcon icon={homeOutline} />
+          <IonText>Home</IonText>
+        </IonTabButton>
+        <IonTabButton>
+          <IonIcon icon={locationOutline} />
+          <IonText>Travel</IonText>
+        </IonTabButton>
+        <IonTabButton>
+          <IonIcon icon={personCircleOutline} />
+          <IonText>Profile</IonText>
+        </IonTabButton>
+      </IonTabBar>
     </IonPage>
   );
 };
-
-export default Home;
