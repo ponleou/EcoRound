919d01cb8d97317b71de0e05dacea4009c7d753e
Ponleou Keo Sok <ponleousk@gmail.com>
2024-12-08 13:27:34 +1100

    fix: adjustments to header and native status bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,15 +25,30 @@ import {
 } from "ionicons/icons";
 import "./Home.css";
 import TabBar from "../components/TabBar";
+import { StatusBar, Style } from "@capacitor/status-bar";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  // Make the status bar blend with header
+  StatusBar.setBackgroundColor({ color: "#ffffff" });
+  const setStatusBarStyleLight = async () => {
+    await StatusBar.setStyle({ style: Style.Light });
+  };
+
+  useEffect(() => {
+    setStatusBarStyleLight();
+  }, []);
+
+
   return (
+    // Header section
     <IonPage>
       <IonHeader className="no-shadow" translucent={true}>
         <IonToolbar>
           <IonTitle color="primary">EcoRound</IonTitle>
           <IonButtons slot="end" class="ion-margin-end" collapse={true}>
-            <IonButton color="primary" shape="round">
+            <IonButton size="small" color="primary" shape="round">
               <IonIcon icon={ellipsisVertical}></IonIcon>
             </IonButton>
           </IonButtons>
@@ -41,16 +56,18 @@ export default function Home() {
       </IonHeader>
 
       <IonContent fullscreen>
+        {/* collapseible header section for iOS */}
         <IonHeader collapse="condense">
-          <IonToolbar>
+          <IonToolbar class="ion-justify-content-center">
             <IonTitle color="primary" size="large">EcoRound</IonTitle>
             <IonButtons slot="end" class="ion-margin-end">
-              <IonButton shape="round">
+              <IonButton size="small" shape="round">
                 <IonIcon icon={ellipsisVertical}></IonIcon>
               </IonButton>
             </IonButtons>
           </IonToolbar>
         </IonHeader>
+
         <p>1</p>
         <p>2</p>
         <p>3</p>
@@ -81,6 +98,7 @@ export default function Home() {
         <p>2</p>
         <p>2</p>
         <p>2</p>
+
       </IonContent>
 
       <TabBar />
