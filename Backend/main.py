48bbfb84c38fc3269586f9789807a9147737aef0
Ponleou Keo Sok <ponleousk@gmail.com>
2024-12-14 19:15:43 +1100

    feat: added route api fetching

--- Backend/main.py
+++ Backend/main.py
@@ -12,17 +12,24 @@ client = ors.Client(api_key)
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/route')
-# def route():
-#     startCoords = request.args.get('startCoords')
+@app.get('/api/route')
+def route():
+    slat = request.args.get('slat')
+    slon = request.args.get('slon')
+    dlat = request.args.get('dlat')
+    dlon = request.args.get('dlon')
 
-#     route = client.directions(
-#         coordinates=[[8.34234,48.23424],[8.34423,48.26424]],
-#         profile='driving-car',
-#         format='geojson'
-#     )
+    profile = request.args.get('profile')
 
-#     return jsonify(route)
+    print(slat, slon, dlat, dlon, profile)
+
+    route = client.directions(
+        coordinates=[[float(slon), float(slat)],[float(dlon), float(dlat)]],
+        profile=profile,
+        format='geojson'
+    )
+
+    return jsonify(route["features"][0])
 
 @app.get('/api/place_name')
 def place():
--- EcoRound/src/function/api.js
+++ EcoRound/src/function/api.js
@@ -11,3 +11,33 @@ export const getPlaceName = async (lat, lon) => {
 
   return response.data;
 };
+
+export const getCarRoute = async (slat, slon, dlat, dlon) => {
+  const response = await CapacitorHttp.request({
+    url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=driving-car`,
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  });
+
+  return response.data;
+};
+
+export const getWalkRoute = async (slat, slon, dlat, dlon) => {
+  const response = await CapacitorHttp.request({
+    url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=foot-walking`,
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  });
+
+  return response.data;
+};
+
+export const getBikeRoute = async (slat, slon, dlat, dlon) => {
+  const response = await CapacitorHttp.request({
+    url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=cycling-regular`,
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  });
+  5;
+  return response.data;
+};
