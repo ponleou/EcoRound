d29f65f631991c90bf6368ebb3ffd8e3d65f3063
Ponleou Keo Sok <ponleousk@gmail.com>
2024-12-16 17:06:22 +1100

    feat: added error handling for api backend

--- EcoRound/src/function/api.js
+++ EcoRound/src/function/api.js
@@ -2,42 +2,49 @@ import { CapacitorHttp } from "@capacitor/core";
 
 const baseUrl = "http://10.141.55.82:5000/api";
 
+function handleResponse(response) {
+  if (response.status >= 300) {
+    throw response.data;
+  }
+  return response.data;
+}
+
 export const getPlaceName = async (lat, lon) => {
-  const response = await CapacitorHttp.request({
+  const reponse = await CapacitorHttp.request({
     url: `${baseUrl}/place_name?lat=${lat}&lon=${lon}`,
     headers: { "Content-Type": "application/json" },
     method: "GET",
   });
 
-  return response.data;
+  return handleResponse(reponse);
 };
 
 export const getCarRoute = async (slat, slon, dlat, dlon) => {
-  const response = await CapacitorHttp.request({
+  const reponse = await CapacitorHttp.request({
     url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=driving-car`,
     headers: { "Content-Type": "application/json" },
     method: "GET",
   });
 
-  return response.data;
+  return handleResponse(reponse);
 };
 
 export const getWalkRoute = async (slat, slon, dlat, dlon) => {
-  const response = await CapacitorHttp.request({
+  const reponse = await CapacitorHttp.request({
     url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=foot-walking`,
     headers: { "Content-Type": "application/json" },
     method: "GET",
   });
 
-  return response.data;
+  return handleResponse(reponse);
 };
 
 export const getBikeRoute = async (slat, slon, dlat, dlon) => {
-  const response = await CapacitorHttp.request({
+  const reponse = await CapacitorHttp.request({
     url: `${baseUrl}/route?slat=${slat}&slon=${slon}&dlat=${dlat}&dlon=${dlon}&profile=cycling-regular`,
     headers: { "Content-Type": "application/json" },
     method: "GET",
   });
-  5;
-  return response.data;
+
+  return handleResponse(reponse);
 };
