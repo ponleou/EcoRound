ed9b0403c33ecfa8c7c728f136bfcfa905cdcfd4
Ponleou Keo Sok <ponleousk@gmail.com>
2024-12-30 19:00:20 +1100

    feat: added customisability to icon text

--- EcoRound/src/components/IconText.tsx
+++ EcoRound/src/components/IconText.tsx
@@ -5,11 +5,17 @@ export default function IconText({
   text,
   iconSize = "large",
   iconColor = "",
+  col = true,
 }) {
   return (
-    <div className="flex flex-col justify-center items-center gap-1 w-fit">
+    <div
+      className={
+        "flex justify-center items-center gap-1 w-fit " +
+        (col ? "flex-col" : "")
+      }
+    >
       <IonIcon color={iconColor} size={iconSize} icon={icon}></IonIcon>
-      <p className="text-xs w-full text-center text-nowrap">
+      <p className={"text-xs text-center text-nowrap " + (col ? "w-full" : "")}>
         <IonText>{text}</IonText>
       </p>
     </div>
